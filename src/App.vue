bbcb94d17515566f88d5cbabc39c4872da0742c1
Glen Sargent <glensarg@hotmail.co.uk>
2020-05-01 14:29:35 +0100

    boilerplate

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Hello Vue 3</h1>
+    <button @click="increase">increment</button>
+    <button @click="decrease">decrement</button>
+    <h2>{{ state.count }}</h2>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref, reactive } from "vue";
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      count: 0
+    })
+    const increase = () => {
+      state.count++
+    }
+
+    const decrease = () => {
+      state.count--
+    }
+
+    return {
+      state,
+      increase,
+      decrease
+    }
+  }
+});
+</script>
